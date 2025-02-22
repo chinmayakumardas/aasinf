e576a2510acc6d470af72943bb8abd089072847e
chinmayakumardas <chinmayakumardas2000@gmail.com>
2025-02-22 13:09:00 +0530

    ok

--- client/postcss.config.mjs
+++ client/postcss.config.mjs
@@ -1,18 +1,9 @@
-// /** @type {import('postcss-load-config').Config} */
-// const config = {
-//   plugins: {
-//     tailwindcss: {},
-//   },
-// };
-
-// export default config;
-y
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},  // <-- Add this
   },
 };
 
 export default config;
+
